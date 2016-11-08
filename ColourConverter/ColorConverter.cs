2b6ee5a5cbf8e3a2201e4a399c8e020c48425896
Fernando Cisneros <fernando.saxofone@gmail.com>
2016-11-08 15:48:21 -0200

    xamarin android

---
+++ ColourConverter/ColorConverter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+
+namespace ColorConverter
+{
+	public class RGB
+	{
+
+		public int r {
+			get;
+			private set;
+		}
+		public int g {
+			get;
+			private set;
+		}
+		public int b {
+			get;
+			private set;
+		}
+
+		public RGB(int r, int g, int b) {
+			this.r = r;
+			this.g = g;
+			this.b = b;
+		}
+
+
+		public static RGB fromHex(string hexColor)
+		{
+			//Remove # if present
+			if (hexColor.IndexOf('#') != -1)
+				hexColor = hexColor.Replace("#", "");
+
+			int red = 0;
+			int green = 0;
+			int blue = 0;
+
+			if (hexColor.Length == 6)
+			{
+				//#RRGGBB
+				red = int.Parse(hexColor.Substring(0, 2), NumberStyles.AllowHexSpecifier);
+				green = int.Parse(hexColor.Substring(2, 2), NumberStyles.AllowHexSpecifier);
+				blue = int.Parse(hexColor.Substring(4, 2), NumberStyles.AllowHexSpecifier);
+
+
+			}
+			else if (hexColor.Length == 3)
+			{
+				//#RGB
+				red = int.Parse(hexColor[0].ToString() + hexColor[0].ToString(), NumberStyles.AllowHexSpecifier);
+				green = int.Parse(hexColor[1].ToString() + hexColor[1].ToString(), NumberStyles.AllowHexSpecifier);
+				blue = int.Parse(hexColor[2].ToString() + hexColor[2].ToString(), NumberStyles.AllowHexSpecifier);
+			}
+			else {
+				red = 255;
+				green = 255;
+				blue = 255;
+			}
+
+
+			return new RGB(red, green, blue);	
+		}
+
+	}
+}
+
---
+++ HexToRgb/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using ColorConverter;
+using Android.Graphics.Drawables;
+
+namespace HexToRgb
+{
+	[Activity(Label = "HexToRgb", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		Button convertButton;
+		EditText hexColorEditText;
+		TextView redtextView;
+		TextView greenTextView;
+		TextView bluetextView;
+		ImageView backgroundImageView;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			convertButton = FindViewById<Button>(Resource.Id.convertButton);
+			hexColorEditText = FindViewById<EditText>(Resource.Id.hexColorEditText);
+			redtextView = FindViewById<TextView>(Resource.Id.redtextView);
+			greenTextView = FindViewById<TextView>(Resource.Id.greenTextView);
+			bluetextView = FindViewById<TextView>(Resource.Id.bluetextView);
+			backgroundImageView = FindViewById<ImageView>(Resource.Id.backgroundImageView);
+
+
+			convertButton.Click+= ConvertButton_Click;
+
+		}
+
+		void ConvertButton_Click(object sender, System.EventArgs e)
+		{
+
+			//Android.Graphics.Color my_colour = new Android.Graphics.Color(255,0,0);
+			//Drawable drawableCircle = Resources.GetDrawable(Resources.GetIdentifier("rgb_color", "drawable", PackageName));
+			GradientDrawable gradientDrawable = (GradientDrawable) backgroundImageView.Background.Current;
+
+			RGB colour = RGB.fromHex(hexColorEditText.Text);
+			Android.Graphics.Color my_colour = new Android.Graphics.Color(colour.r, colour.g, colour.b);
+			gradientDrawable.SetColor(my_colour);
+
+			redtextView.Text = colour.r.ToString();
+			greenTextView.Text = colour.g.ToString();
+			bluetextView.Text = colour.b.ToString();
+
+		}
+	}
+}
+
+
